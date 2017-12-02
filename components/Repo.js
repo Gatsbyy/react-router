6f61bd021b97a47416a38f86a6c1555247721842
Gatsbyy <784007667@qq.com>
2017-12-02 15:39:41 +0800

    update
    update

--- components/Repo.js
+++ components/Repo.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 
 class Repo extends Component {
   render() {
-    console.log('Repo');
-    console.log(this.props);
     return(
       <div>{this.props.match.params.userName}</div>
     )
